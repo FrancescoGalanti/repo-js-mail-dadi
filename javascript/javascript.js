55bf36e1009b4d89f0841c51a4dda2a6c5784f56
FrancescoGalanti <galanti_francesco>
2020-10-15 14:45:50 +0200

    Primo esercizio fatto

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -9,11 +9,25 @@
 
  // seconda variabile //
 
- var emailuser = prompt("Inserisci la tua email")
+ var emailuser = prompt("Inserisci la tua email").toLowerCase();
 
  // controllo//
 
+ var userfound = false;
 
 
+ for( var i = 0;  i < email.length; i++ ){
 
- for( var i = 0; var i < emailuser.lenght; i++ )
+    var item = email[i];
+
+    if( item === emailuser) {
+        userfound = true;
+    }
+
+ }
+
+ if(userfound == true) {
+   console.log("Utente trovato, procedi con l\"accesso");
+ } else(
+    console.log("errore di autenticazione")
+ )
